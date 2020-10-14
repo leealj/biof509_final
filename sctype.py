dcf4578df14d04a79c4a3ccac64defca2e106f53
leealj <leeaxj@gmail.com>
2020-10-13 21:02:41 -0400

    train-test split to its own method

--- sctype.py
+++ sctype.py
@@ -5,9 +5,13 @@ from sklearn.model_selection import train_test_split
 
 class sctype:
     def __init__(self, csv, test_size, random_state):
-        df = pd.read_csv(csv)
+        self.df = pd.read_csv(csv)
+        # this data-importing step will be updated once I figure out the best way to
+        # accomplish it.
+
+    def split(self, test_size, random_state):
         train, test = train_test_split(
-            df, test_size=test_size, random_state=random_state)
+            self.df, test_size=test_size, random_state=random_state)
         self.train_labels = train.iloc[]
         self.train_data = train.iloc[]
         self.test_labels = test.iloc[]
